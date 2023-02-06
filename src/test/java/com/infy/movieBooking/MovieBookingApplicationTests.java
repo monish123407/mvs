c1c4e7ea10edb7bb0aa81acad6923a916f6772e8
monish123407 <54147751+monish123407@users.noreply.github.com>
2023-02-06 20:42:43 +0530

    first commit
    first commit

---
+++ src/test/java/com/infy/movieBooking/MovieBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.movieBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
